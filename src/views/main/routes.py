e9b82da25db3f96fa9435b25ab5c773a37e5c0d1
ბექა <beka.kopadze.2@iliauni.edu.ge>
2025-02-20 22:39:23 +0400

    changing something, i dont know any more

--- src/views/main/routes.py
+++ src/views/main/routes.py
@@ -7,6 +7,26 @@ from src.config import Config
 TEMPLATES_FOLDER = path.join(Config.BASE_DIR, 'templates', 'main')
 main_bp = Blueprint('main', __name__, template_folder=TEMPLATES_FOLDER)
 
+poem = """ჩემო კალამო, ჩემო კარგო, რად გვინდა ტაში?
+        რასაც ვმსახურებთ, მას ერთგულად კვლავ ვემსახუროთ,
+        ჩვენ წმინდა სიტყვა უშიშარად მოვფინოთ ხალხში
+        ბოროტთ საკლავად, — მათ სულთ-ხდომის სეირს ვუყუროთ.
+
+        თუ კაცმა ვერ სცნო ჩვენი გული, ხომ იცის ღმერთმა,
+        რომ წმინდა არის განზრახვა და სურვილი ჩვენი:
+        აგვიყოლია სიყრმიდანვე ჩვენ ქართვლის ბედმა
+        და დაე გვძრახონ, — ჩვენ მის ძებნით დავლიოთ დღენი.
+
+        ჩემზედ ამბობენ: „ის სიავეს ქართვლისას ამბობს,
+        ჩვენს ცუდს არ მალავს, ეგ ხომ ცხადი სიძულვილია!”
+        ბრიყვნი ამბობენ, კარგი გული კი მაშინვე სცნობს —
+        ამ სიძულვილში რაოდენიც სიყვარულია!"""
+
+def format_word(word):
+    return "".join(f'<span class="letter">{char}</span>' for char in word)
+
+poem = format_word(poem)
+
 @main_bp.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', poem=poem)
