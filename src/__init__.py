e25216d2d53bf23cba5477e92627552aa50f47b8
ბექა <beka.kopadze.2@iliauni.edu.ge>
2025-03-07 18:44:30 +0400

    made javascript code more efficient, fixed backspace bug

--- src/__init__.py
+++ src/__init__.py
@@ -32,20 +32,20 @@ def register_blueprints(app):
 def register_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
-    admin.init_app(app)
-
-    class PoemAdmin(ModelView):
-        form_columns = ['name', 'verse', 'author']  # Include author field
-        form_ajax_refs = {
-            'author': {
-                'fields': ['name']  # Search authors by name
-            }
-        }
-    class AuthorAdmin(ModelView):
-        form_columns = ['name']
-
-    admin.add_view(PoemAdmin(Poem, db.session))
-    admin.add_view(AuthorAdmin(Author, db.session))
+    # admin.init_app(app)
+    #
+    # class PoemAdmin(ModelView):
+    #     form_columns = ['name', 'verse', 'author']  # Include author field
+    #     form_ajax_refs = {
+    #         'author': {
+    #             'fields': ['name']  # Search authors by name
+    #         }
+    #     }
+    # class AuthorAdmin(ModelView):
+    #     form_columns = ['name']
+    #
+    # admin.add_view(PoemAdmin(Poem, db.session))
+    # admin.add_view(AuthorAdmin(Author, db.session))
 
 
 def register_commands(app):
